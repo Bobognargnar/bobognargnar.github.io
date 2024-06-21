2f9ccfd070e027239da53a774d9116c921889e81
Fabrizio La Rosa <fbr.larosa@gmail.com>
2024-06-21 17:00:20 +0200

    feat: Add tab template

---
+++ src/components/EssentialTab.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-tab
+    :icon=icon
+    name="something"
+  >
+  </q-tab>
+</template>
+
+<script>
+
+export default {
+  name: 'EssentialLink',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+
+    caption: {
+      type: String,
+      default: ''
+    },
+
+    target: {
+      type: String,
+      default: '_self'
+    },
+
+    icon: {
+      type: String,
+      default: ''
+    },
+    routeName: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
