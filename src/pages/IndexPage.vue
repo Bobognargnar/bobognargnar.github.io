3c607385f85f37316edbb46a27000c887c1803c1
Fabrizio La Rosa <fbr.larosa@gmail.com>
2024-06-21 16:59:56 +0200

    feat: Add index page content

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,25 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-card class="my-card">
+  <q-page padding class="">
+    <div class="card-container">
+    <q-card bordered class="">
       <q-card-section>
-        {{ lorem }}
+        <div class="text-h6">About Me</div>
+      </q-card-section>
+      <q-card-section>
+        {{ bio }}
       </q-card-section>
     </q-card>
+    <q-separator vertical inset></q-separator>
+    <q-card bordered class="">
+      <q-card-section>
+        <div class="text-h6">Experience</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+      <q-card-section>
+        {{ bio }}
+      </q-card-section>
+    </q-card>
+  </div>
   </q-page>
 </template>
 
@@ -14,8 +29,13 @@ export default {
   name: 'IndexPage',
   setup () {
     return {
-      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+      bio: 'I am a software engineer with 9 years of experience working multiple roles in the oil&gas andmedia fields. I love writing beautiful, clean code and solving complex real world problems. I’m not afraid to challenge myself and go outside of my comfort zone.'
     }
   }
 }
 </script>
+
+<style scoped>
+.card-container {
+}
+</style>
